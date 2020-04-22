819f657ffe02043b87261d8a99a05d0971f45edc
nat236919 <nuttaphat.aruno@gmail.com>
2020-04-22 14:57:01 +0800

    create grids for total data

--- covid19-dashboard/src/components/Total.vue
+++ covid19-dashboard/src/components/Total.vue
@@ -1,9 +1,14 @@
 <template>
-    <div id="show-total">
+    <div id="show-total" class="text-center">
         <h2> Total Data </h2>
-        <li v-bind:key="value" v-for="(value, index) in total.data">
-             {{index}} {{value}}
-        </li>
+        <div class="row">
+            <div v-bind:key="value" v-for="(value, index) in total.data">
+                <div class="card col border-dark d-flex justify-content-center" style="width: 16rem; margin-left: 5px;">
+                    <h4>{{index}}</h4>
+                    <div class="card-body"><p class="card-text">{{value}}</p></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,4 +32,7 @@ export default {
 </script>
 
 <style scoped>
+    h4 { text-transform: capitalize; }
+    .card { transition: all .2s ease-in-out; }
+    .card:hover { transform: scale(0.9); }
 </style>
