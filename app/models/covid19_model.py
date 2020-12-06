cf57c159f3ba54990b3ef21382bfa3e8815c93b4
Nuttaphat <nuttaphat.aruno@gmail.com>
2020-12-06 20:54:41 +0800

    refactor model and add method

--- app/models/covid19_model.py
+++ app/models/covid19_model.py
@@ -6,6 +6,8 @@ DATE: 01-DEC-2020
 """
 # Import libs
 import requests
+from typing import Dict, Any
+
 import pandas as pd
 
 
@@ -16,7 +18,22 @@ class Covid19Model:
         self.api_endpoint = 'https://covid19.nuttaphat.com/v2/'
     
     def get_current(self) -> pd.DataFrame:
-        res = requests.get(self.api_endpoint+'current').json()
-        data = res.get('data')
-        df = pd.DataFrame(data)
+        df = self._get_data(self.api_endpoint+'current')
         return df
+    
+    def get_total(self) -> pd.DataFrame:
+        df = self._get_data(self.api_endpoint+'total')
+        return df
+    
+    def _get_data(self, url: str) -> Dict[str, Any]:
+        if not url:
+            return []
+        res = requests.get(url).json()
+        data = res.get('data')
+        df = self._create_df(data)
+        return df
+    
+    def _create_df(self, data: Dict[str, Any]) -> pd.DataFrame:
+        if not data:
+            data = {}
+        return pd.DataFrame(data)
