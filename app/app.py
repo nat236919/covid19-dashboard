2a628b84d7b3b51c64a8553b4207d282728705fb
nat236919 <nuttaphat.aruno@gmail.com>
2020-12-08 14:51:01 +0800

    change graph layouts

--- app/app.py
+++ app/app.py
@@ -16,28 +16,38 @@ from dash.dependencies import Input, Output
 from models.covid19_model import Covid19Model
 from utils.helper import DashHelper, DataFrameHelper
 
-# Init app and modules
+
+# Init modules
 dataframe_helper = DataFrameHelper()
 dash_helper = DashHelper()
 covid19_model = Covid19Model()
+
+
+# Init app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 # Prepare data and Create graph
-# Total data
+## Total data (dataframe and charts)
 df_total = covid19_model.get_total()
 df_total = dataframe_helper.clean_df_total(df_total)
+
 fig_pie = px.pie(df_total, values='total', names='category')
 
 
-# Current data
+## Current data (dataframe and charts)
 df_current = covid19_model.get_current()
 df_current = dataframe_helper.clean_df_current(df_current)
-fig_choropleth = px.choropleth(df_current, locations='country_code_iso3',  color='confirmed', hover_name='location', title='World-Wide Infection')
-fig_treemap = px.treemap(df_current, path=[px.Constant('world'), 'continent', 'location'], values='confirmed', color='active', hover_data=['country_code_iso3'],
-                 color_continuous_scale='RdBu', color_continuous_midpoint=np.average(df_current['active'], weights=df_current['confirmed']))
-fig_scatter = px.scatter(df_current, x='deaths', y='active',
+
+fig_choropleth = px.choropleth(df_current, locations='country_code_iso3', color='confirmed',
+                hover_name='location', title='World-Wide Infection')
+
+fig_treemap = px.treemap(df_current, path=[px.Constant('world'), 'continent', 'location'], values='confirmed',
+                color='active', hover_data=['country_code_iso3'], color_continuous_scale='RdBu',
+                color_continuous_midpoint=np.average(df_current['active'], weights=df_current['confirmed']))
+
+fig_bubble = px.scatter(df_current, x='deaths', y='active',
                  size='confirmed', color="continent", hover_name='location',
                  log_x=True, size_max=80)
 
@@ -55,9 +65,10 @@ app.layout = html.Div([
     # Tabs
     html.Div([
         dcc.Tabs(id="tabs", value='tab-choropleth', children=[
-            dcc.Tab(label='World-Wide Infection',   value='tab-choropleth'),
-            dcc.Tab(label='Total Data - Pie Chart', value='tab-pie'),
-            dcc.Tab(label='Total Data - Table',     value='tab-table'),
+            dcc.Tab(label='World-Wide Infection (Map)',             value='tab-choropleth'),
+            dcc.Tab(label='World-Wide Infection (Bubble Chart)',    value='tab-bubble'),
+            dcc.Tab(label='Total Data - Pie Chart',                 value='tab-pie'),
+            dcc.Tab(label='Total Data - Table',                     value='tab-table'),
         ]),
         html.Div(id='tabs-content')
     ], style={'margin': 'auto', 'width': '60%', 'padding': '10px'}),
@@ -66,10 +77,6 @@ app.layout = html.Div([
     dcc.Graph(
         id='treemap_graph',
         figure=fig_treemap
-    ),
-    dcc.Graph(
-        id='bubble_graph',
-        figure=fig_scatter
     )
 ])
 
@@ -82,6 +89,13 @@ def render_content(tab):
             dcc.Graph(
                 id='choropleth_graph',
                 figure=fig_choropleth
+            )
+        ])
+    elif tab == 'tab-bubble':
+        return html.Div([
+            dcc.Graph(
+                id='bubble_graph',
+                figure=fig_bubble
             )
         ])
     elif tab == 'tab-pie':
